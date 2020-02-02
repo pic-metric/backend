9fc75d63e5dcadb5d1b3f2133bc281643a63c1e5
Moses Samuel <mosamuel98@gmail.com>
2020-02-01 20:13:18 -0800

    annotate helper functions

--- util.js
+++ util.js
@@ -1,9 +1,11 @@
+// Promise any -> Array Promise
 function withCatch(promise) {
     return promise
         .then(data => [null, data])
         .catch(err => [err])
 }
 
+// Array String -> Object -> Object
 function validateFields (fields=[], obj={}) {
     return fields.every(field => field in obj)
 }
