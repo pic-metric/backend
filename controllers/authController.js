bcb922a7234009eb4bf250f6984780d9642d8bbd
Moses Samuel <mosamuel98@gmail.com>
2020-02-03 11:16:17 -0800

    add register functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,32 @@
-const usersModel = require('../models/usersModel')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
-class AuthController {}
+const { JWT_SECRET } = require('../config/secrets')
+const Users = require('../models/usersModel')
+const { catchErrors } = require('../helpers')
+
+class AuthController {
+   static async register(req, res, next) {
+       // asyncronously hash the password before storing it in the database.
+        bcrypt.hash(req.body.password, 8, async (err, encryptedPw) => {
+            if (err) {
+                next (new Error('there was an issue with hashing the password'))
+            } else {
+                try {
+                    // Insert the user with the newly hashed password into the system.
+                    const newUser = await Users.create({
+                        email: req.body.email,
+                        full_name: req.body.full_name,
+                        hashed_password: encryptedPw
+                    })
+                    res.status(200).json(newUser)
+                    
+                } catch(err) {
+                    next(new Error('there was an issue inserting the user into the database.'))
+                }
+            }
+        })
+   }
+}
 
 module.exports = AuthController
