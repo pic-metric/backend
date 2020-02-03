d065207d79f7fb42bd493c3a5acffe27684cf49f
Moses Samuel <mosamuel98@gmail.com>
2020-02-03 10:21:31 -0800

    outline ther routes for the pics router

--- routes/picsRouter.js
+++ routes/picsRouter.js
@@ -5,4 +5,22 @@
 const router = require('express').Router({mergeParams: true})
 const { responseForUnfinishedEndpoint } = require('../helpers')
 
+
+// GET https://bw-pic-metric.herokuapp.com/api/pics
+// Retrieve all pictures in the system.
+router.get('/pics', responseForUnfinishedEndpoint)
+
+// GET https://bw-pic-metric.herokuapp.com/api/pics/:user_id
+// Retrieve all pictures for a particular user.
+router.get('/pics/:user_id', responseForUnfinishedEndpoint)
+
+
+// POST https://bw-pic-metric.herokuapp.com/api/pics
+// Upload a picture.
+router.post('/pics', responseForUnfinishedEndpoint)
+
+// DELETE https://bw-pic-metric.herokuapp.com/api/pics/:pic_id
+// Remove a picture from the system.
+router.delete('/pics/:pic_id', responseForUnfinishedEndpoint)
+
 module.exports = router
