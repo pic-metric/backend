735515ac6fcadcdeb2f457f4c3c42352ce00202e
Moses Samuel <mosamuel98@gmail.com>
2020-02-03 10:55:41 -0800

    add a method for retrieving all users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,26 @@
-const usersModel = require('../models/usersModel')
+/**
+ * Imports 
+ */
+const Users = require('../models/usersModel')
+const { catchErrors } = require('../helpers')
 
-class UsersController {}
+
+/**
+ * Main
+ */
+
+class UsersController {
+    static async allUsers(req, res, next) {
+        const [err, users] = await catchErrors(Users.all())
+
+        if (err) {
+            next(err)
+        } else if (Object.keys(users).length === 0) {
+            next(new Error('No users exist in the database yet.'))
+        } else {
+            res.status(200).json(users)
+        }
+    } 
+}
 
 module.exports = UsersController
