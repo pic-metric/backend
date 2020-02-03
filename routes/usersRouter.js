b11ce8d3de62206692c571e7521a98a5c73f4426
Moses Samuel <mosamuel98@gmail.com>
2020-02-02 17:42:22 -0800

    outline endpoints for users

---
+++ routes/usersRouter.js
@@ -0,0 +1,33 @@
+/**
+ * Imports
+ */
+
+const router = require('express').Router({mergeParams: true})
+const usersModel = require('../models/usersModel')
+const { responseForUnfinishedEndpoint } = require('../helpers')
+
+
+/**
+ * Main
+ */
+
+
+// GET /api/users 
+// Retrieve all users in the system.
+router.get('/users', responseForUnfinishedEndpoint)
+
+
+// PUT /api/users/:user_id
+// Update a user's information.
+router.put('/users/:user_id', responseForUnfinishedEndpoint)
+
+// DELETE /api/users/:user_id
+// Delete a user from the system.
+router.delete('/api/users/:user_id', responseForUnfinishedEndpoint)
+
+
+
+
+
+
+
