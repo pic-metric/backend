b96a1e4ff2a604071349d560ce74456e33ffac72
Moses Samuel <mosamuel98@gmail.com>
2020-02-02 16:32:36 -0800

    add a helper for responding to unfinished endpoints

--- util.js
+++ util.js
@@ -10,7 +10,12 @@ function validateFields (fields=[], obj={}) {
     return fields.every(field => field in obj)
 }
 
+function responseForUnfinishedEndpoint(req, res) {
+    res.status(200).send("This endpoint is still a work in progress.")
+}
+
 module.exports = {
     withCatch,
-    validateFields
+    validateFields,
+    responseForUnfinishedEndpoint
 }
