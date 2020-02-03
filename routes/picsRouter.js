a78694ebc754b2f0822ed4bdc44b1d73c3c7aeff
Moses Samuel <mosamuel98@gmail.com>
2020-02-03 10:24:23 -0800

    fix routes for pics
    (there was a duplicate word in the path)

--- routes/picsRouter.js
+++ routes/picsRouter.js
@@ -8,19 +8,19 @@ const { responseForUnfinishedEndpoint } = require('../helpers')
 
 // GET https://bw-pic-metric.herokuapp.com/api/pics
 // Retrieve all pictures in the system.
-router.get('/pics', responseForUnfinishedEndpoint)
+router.get('/', responseForUnfinishedEndpoint)
 
 // GET https://bw-pic-metric.herokuapp.com/api/pics/:user_id
 // Retrieve all pictures for a particular user.
-router.get('/pics/:user_id', responseForUnfinishedEndpoint)
+router.get('/:user_id', responseForUnfinishedEndpoint)
 
 
 // POST https://bw-pic-metric.herokuapp.com/api/pics
 // Upload a picture.
-router.post('/pics', responseForUnfinishedEndpoint)
+router.post('/', responseForUnfinishedEndpoint)
 
 // DELETE https://bw-pic-metric.herokuapp.com/api/pics/:pic_id
 // Remove a picture from the system.
-router.delete('/pics/:pic_id', responseForUnfinishedEndpoint)
+router.delete('/:pic_id', responseForUnfinishedEndpoint)
 
 module.exports = router
