17919b92f0f8416edf730b036f1c9139298c555b
Moses Samuel <mosamuel98@gmail.com>
2020-02-03 15:43:15 -0800

    add a "without" helper function

--- helpers.js
+++ helpers.js
@@ -15,8 +15,29 @@ function responseForUnfinishedEndpoint(req, res) {
     res.status(200).send("This endpoint is still a work in progress.")
 }
 
+// String -> Object -> Object
+function without (prop, obj) {
+    const typeErrorMessage = "You must supply either a string or an array of strings as the prop to filter out."
+    switch (typeof prop) {
+        case "string":
+            return filterObj( function predicate(key, val) { return key !== prop }, obj )
+        case "object": 
+            if (Array.isArray(prop)) {
+                return filterObj(function predicate(key, val) { 
+                    return !prop.includes(key) 
+                }, obj)
+            } else {
+                throw new TypeError(typeErrorMessage)
+            }
+        default: 
+            throw new TypeError(typeErrorMessage)
+    }
+}
+
+
 module.exports = {
     catchErrors,
     validateFields,
-    responseForUnfinishedEndpoint
+    responseForUnfinishedEndpoint,
+    without
 }
