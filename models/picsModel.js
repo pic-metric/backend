95930284ede28a8e058b1c245a0e13b48f625b5d
Moses Samuel <mosamuel98@gmail.com>
2020-02-04 12:20:38 -0800

    add a few methods in the pics model

--- models/picsModel.js
+++ models/picsModel.js
@@ -1,3 +1,29 @@
 const db = require('../database/dbClient')
 
-module.exports = {}
+module.exports = {
+    // Promise (Array Obj)
+    all() {
+        return db('pics')
+    },
+
+    // String -> Promise (Array Obj)
+    allForUser(userId) {
+        return db('pics').where({id: userId})
+    },
+
+    // Most important endpoint in the entire app.
+    // String -> Promise Object
+    processAndCreate(url) {
+        /*
+        Store the url in the db.
+        Then query the DS ednpoint to process the image.
+        Once the image is processed, write to the attributes
+        table all the attributes for the image. (Attributes.create())
+         */
+         
+    },
+
+    delete(picId) {
+        return db('users').delete().where({id: picId})
+    }
+}
