b4fdf144c9982a7622a4e514c499f70e6046da7f
Moses Samuel <mosamuel98@gmail.com>
2020-02-04 13:03:47 -0800

    include user_id param in post route of pics router

--- routes/picsRouter.js
+++ routes/picsRouter.js
@@ -4,7 +4,10 @@
 
 const router = require('express').Router({mergeParams: true})
 const { responseForUnfinishedEndpoint } = require('../helpers')
+const requireAuthentication = require('../middleware/authenticate')
 
+// Require a valid json web token on all routes
+// router.all(requireAuthentication)
 
 // GET https://bw-pic-metric.herokuapp.com/api/pics
 // Retrieve all pictures in the system.
@@ -17,7 +20,7 @@ router.get('/:user_id', responseForUnfinishedEndpoint)
 
 // POST https://bw-pic-metric.herokuapp.com/api/pics
 // Upload a picture.
-router.post('/', responseForUnfinishedEndpoint)
+router.post('/:user_id', responseForUnfinishedEndpoint)
 
 // DELETE https://bw-pic-metric.herokuapp.com/api/pics/:pic_id
 // Remove a picture from the system.
