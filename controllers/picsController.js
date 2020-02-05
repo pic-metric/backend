63308c26b4a722b8d3985559b4546d7184cedd44
Moses Samuel <mosamuel98@gmail.com>
2020-02-05 15:52:24 -0800

    implement controller logic + routing for uploading pics

--- controllers/picsController.js
+++ controllers/picsController.js
@@ -2,6 +2,7 @@ const Pics = require('../models/picsModel')
 const Attributes = require('../models/attributesModel')
 const axios = require('axios')
 const { catchErrors } = require('../helpers')
+const fs = require('fs')
 
 class PicsController {
     static async getAllPics(req, res, next) {
@@ -40,8 +41,14 @@ class PicsController {
 
     // Grab the image from the frontend and store it in the db.
     static async processAndCreate(req, res, next) {
+
+        console.log('========REQ.FILE=======', req.file, '\n\n')
+        console.log('========REQ.BODY==========', req.body)
+        
+        let byteData = fs.readFileSync(req.file.path)
+        
         // Store the url in the db.
-        Pics.create(req.params.user_id, req.file)
+        Pics.create(req.params.user_id, byteData)
             .then(ids => {
                 res.status(200).send("Successfully saved photo. Processing image now...")
                 // call the flask api
