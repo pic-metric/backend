87e28e0928f1cfc3b6889a3b157a2d5c32d785fb
Moses Samuel <mosamuel98@gmail.com>
2020-02-05 13:51:26 -0800

    add functionality for retrieving all attributes for a pic

--- controllers/attributesController.js
+++ controllers/attributesController.js
@@ -1,5 +1,16 @@
 const attributesModel = require('../models/attributesModel')
+const { catchErrors } = require('../helpers')
 
-class AttributesController {} 
+class AttributesController {
+    static async getAllForPic(req, res, next) {
+        const [err, pics] = await catchErrors(attributesModel.getAllForPic(req.params.pic_id))
+
+        if (err) {
+            next(err)
+        } else {
+            res.status(200).json(pics)
+        }   
+    }
+} 
 
 module.exports = AttributesController
