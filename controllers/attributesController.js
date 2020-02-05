2d2204c36f1d4779d7d43123e75037eabff60551
Moses Samuel <mosamuel98@gmail.com>
2020-02-05 14:05:15 -0800

    fix typo

--- controllers/attributesController.js
+++ controllers/attributesController.js
@@ -3,12 +3,12 @@ const { catchErrors } = require('../helpers')
 
 class AttributesController {
     static async getAllForPic(req, res, next) {
-        const [err, pics] = await catchErrors(attributesModel.getAllForPic(req.params.pic_id))
+        let [err, attributes] = await catchErrors(attributesModel.getAllForPic(req.params.pic_id))
 
         if (err) {
             next(err)
         } else {
-            res.status(200).json(pics)
+            res.status(200).json(attributes)
         }   
     }
 } 
