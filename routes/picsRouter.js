c542b446b66606209db812f65aaeb4e21f18dfd2
Moses Samuel <mosamuel98@gmail.com>
2020-02-06 11:50:31 -0800

    multer debugging

--- routes/picsRouter.js
+++ routes/picsRouter.js
@@ -35,7 +35,8 @@ router.get('/for/:user_id', PicsController.getAllPicsForUser)
 
 // POST https://bw-pic-metric.herokuapp.com/api/:user_id
 // Upload a picture.
-router.post('/:user_id', upload.single('pic'),  PicsController.processAndCreate)
+// router.post('/:user_id', upload.single('pic'),  PicsController.processAndCreate)
+router.post('/:user_id', upload.none(),  PicsController.processAndCreate)
 
 // DELETE https://bw-pic-metric.herokuapp.com/api/pics/:pic_id
 // Remove a picture from the system.
